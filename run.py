aef15471f6f6b97533df10f28319241ea9e49687
Karel Bilek <kb@karelbilek.com>
2017-11-22 16:15:26 +0100

    Some python magic

--- run.py
+++ run.py
@@ -13,17 +13,17 @@ lines = [line.rstrip('\n') for line in open('addresses')]
 def f(line):
     txids = rpc_connection.getaddresstxids(line);
     for idd in txids:
-		  rpc_connection.getrawtransaction(idd)
+      rpc_connection.getrawtransaction(idd)
 
 
 while True:
 
-	start = time.time()
-	pool = Pool(processes=4)              # start 4 worker processes
-	pool.map(f, lines)
+  start = time.time()
+  pool = Pool(processes=4)              # start 4 worker processes
+  pool.map(f, lines)
 
-	end = time.time()
-	sec = (end - start)
-	persec = float(len(lines))/sec
-	print(persec)
+  end = time.time()
+  sec = (end - start)
+  persec = float(len(lines))/sec
+  print(persec)
 
