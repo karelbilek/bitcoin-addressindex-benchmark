1ae504aaa14bdba5f8264d39bea8e4f01c6cbbb2
Karel Bilek <kb@karelbilek.com>
2017-11-22 16:27:26 +0100

    edits

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-run.py for tests
+For tests:
+
+`
+pip3 install python-bitcoinrpc
+python3 run.py
+`
 
 see extract/ for extracting addresses from blockchain
--- run.py
+++ run.py
@@ -2,28 +2,32 @@ from multiprocessing import Pool, TimeoutError
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import time
 
-
+def chunk(l, n, i):
+    return l[i*n:(i+1) * n]
 
 rpc_user="bitcoin"
 rpc_password="local321"
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%(rpc_user, rpc_password))
+rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(rpc_user, rpc_password))
 
 lines = [line.rstrip('\n') for line in open('addresses')]
 
 def f(line):
-    txids = rpc_connection.getaddresstxids(line);
-    for idd in txids:
-      rpc_connection.getrawtransaction(idd)
+  txids = rpc_connection.getaddresstxids(line);
+  for idd in txids:
+    rpc_connection.getrawtransaction(idd)
 
 
+i = 0
 while True:
+  mychunk = chunk(lines, 1000, i)
+  i+1
 
   start = time.time()
   pool = Pool(processes=4)              # start 4 worker processes
-  pool.map(f, lines)
+  pool.map(f, mychunk)
 
   end = time.time()
   sec = (end - start)
-  persec = float(len(lines))/sec
+  persec = float(len(mychunk))/sec
   print(persec)
 
